82df1795a48f9eabecb34913f8b205b9db92cf17
nik01101 <nikcolascanessaarguedas@gmail.com>
2026-01-14 11:37:28 -0500

    10

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
+
+// Configure Amplify BEFORE importing any components to avoid race conditions 
+// where generateClient is called before the configuration is loaded.
+Amplify.configure(outputs)
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Amplify } from 'aws-amplify'
-import outputs from '../amplify_outputs.json'
-
-Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
